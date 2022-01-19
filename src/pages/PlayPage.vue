047c1bd3f7e2e133239b3ba9223e1da752a69a52
kawabata <s1931115@s.konan-u.ac.jp>
2022-01-19 15:27:01 +0900

    [UPDATE]topPageの完成

---
+++ src/pages/PlayPage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import { useRoute } from 'vue-router';
+
+  const route = useRoute()
+  const state = reactive({
+    name: route.query.name
+  })
+</script>
+<template>
+  <div>
+    <h1>プレイ画面</h1>
+    <p>UserName:{{state.name}}</p>
+  </div>
+</template>
+<style scoped></style>
--- src/pages/TopPage.vue
+++ src/pages/TopPage.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive } from "vue";
+import { useRouter } from "vue-router";
 
-  const state = reactive({name: ''})
-  const startGame = ()=>{
-    window.alert(`UserName:${state.name}`)
-  }
+const router = useRouter();
+const state = reactive({ name: "" });
+const startGame = () => {
+  router.push({ path: "/play", query: { name: state.name } });
+};
 </script>
 <template>
   <div class="container mx-auto my-20 text-center">
     <h1 class="text-3xl font-bold">トップ画面</h1>
-    <p class="mt-10">{{state.name}}</p>
+    <p class="mt-10">{{ state.name }}</p>
     <div class="flex flex-col mx-auto items-center mt-10 w-80">
       <input
         v-model="state.name"
-        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
-        text-center
-        "
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center"
         type="text"
         placeholder="名前を入力してください(10文字以内)"
         maxlength="10"
-      >
+      />
       <button
         @click="startGame"
         class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-10"
